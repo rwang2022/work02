c588380e593a7e6cac36fb2f6d827a2f2f4735a9
rwang2022 <90988076+rwang2022@users.noreply.github.com>
2021-10-19 23:28:35 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    printf("int: %d\n", 3);
+    printf("float: %f\n", 3.33);
+    printf("float to 3 places: %0.3f\n", 3.33);
+    printf("char: %c\n", '3');
+    return 0;
+}
+
